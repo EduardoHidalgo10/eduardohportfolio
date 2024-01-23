47fdbc1a774312e53afb27421266cbca85fdce6b
Eduardo Hidalgo <eduardohidalgogarcia0210@gmail.com>
2024-01-23 19:45:10 -0400

    mobile navmenu

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    darkMode:'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            backgroundImage: {
+                "close-menu": "url('../../assets/imgs/icon-close.svg')",
+                "open-menu": "url('../../assets/imgs/icon-hamburger.svg')",
+            },
+        },
+    },
+    plugins: [],
+};
